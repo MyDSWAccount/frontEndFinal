be211c5cfd9820a8cda1194de09fae9c225c8fc6
MyDSWAccount <77691217+MyDSWAccount@users.noreply.github.com>
2021-02-10 10:17:27 -0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
         $("#cue").click(function(){
                 $(".CSSEdit").slideToggle("slow");
         });
+        $("#jsq").click(function(){
+                $("#fi").fadeIn("slow", function(){
+                        $();
+        });
 });
